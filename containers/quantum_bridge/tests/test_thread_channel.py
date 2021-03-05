cececfba077dae28e5123d34112d2e2c84d11eed
Stephen Diadamo <stephen.diadamo@gmail.com>
2021-03-05 12:26:17 +0100

    Refactoring a bit

---
+++ containers/quantum_bridge/tests/test_thread_channel.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+class TestChannel(unittest.TestCase):
+
+    # Runs before all tests
+    @classmethod
+    def setUpClass(cls) -> None:
+        pass
+
+    # Runs after all tests
+    @classmethod
+    def tearDownClass(cls) -> None:
+        pass
+
+    # Runs before each test
+    def setUp(self) -> None:
+        pass
+
+    # Runs after each test
+    def tearDown(self) -> None:
+        pass
+
+    def test_instantiation(self):
+        pass
+
+    def test_init(self):
+        self.assertTrue(True)
